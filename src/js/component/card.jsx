c64a47a450e4431b07a4a715527de7c71db47ef0
Miguel Gil F <67928892+Miguel-web@users.noreply.github.com>
2023-07-21 22:38:32 +0000

    render card , style right sintax on lineal css

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -2,15 +2,13 @@ import React from "react";
 
 export const Card = () => {
 	return (
-		<div className="card-group">
-  <div className="card">
+    <div className="card" style={{width: "18rem"}}>
     <img src="..." className="card-img-top" alt="..."/>
     <div className="card-body">
       <h5 className="card-title">Card title</h5>
-      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" className="btn btn-primary">Go somewhere</a>
     </div>
-  </div>
   </div>
 	);
 };
