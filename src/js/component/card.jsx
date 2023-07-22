b00d45ca8a241f5089847987c34c06624c4c8120
Miguel Gil F <67928892+Miguel-web@users.noreply.github.com>
2023-07-22 23:35:04 +0000

    a to button , onclick event

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
-export const Card = () => {
+export const Card = (props) => {
 	return (
-    <div className="card" style={{width: "18rem"}}>
+    <div className="card" style={{maxWidth: "18rem"}}>
     <img src="..." className="card-img-top" alt="..."/>
     <div className="card-body">
       <h5 className="card-title">Card title</h5>
       <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" className="btn btn-primary">Go somewhere</a>
+      <button className={`btn btn-dark text-light`} onClick={() => {
+
+
+      }}>GO!</button>
     </div>
   </div>
 	);
