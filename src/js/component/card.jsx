d90df21e09b3f5c5755b07cdd42e4a0298b6a196
Miguel-web <miguelgilf@gmail.com>
2023-07-25 00:44:59 +0000

    data name , destructure  card componen

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -6,7 +6,7 @@ export const Card = ({ data }) => {
       <div className="card h-100">
         <img src="..." className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
+          <h5 className="card-title">{data.name}</h5>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           <button className={`btn btn-dark text-light`} onClick={() => {
           }}>GO!</button>
