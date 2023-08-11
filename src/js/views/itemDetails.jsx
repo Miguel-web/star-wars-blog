844c288387ca8f44bbe98efc7352ebb5172353c0
Miguel-web <miguelgilf@gmail.com>
2023-08-11 04:11:02 +0000

    falta img en details

--- src/js/views/itemDetails.jsx
+++ src/js/views/itemDetails.jsx
@@ -15,17 +15,12 @@ export const ItemDetails = (data, type) => {
             <div className="card mb-3" style={{ maxWidth: "540px" }}>
                 <div className="row g-0">
                     <div className="col-md-4">
-                        <img src={type == "planets" && data.uid == 1
-                            ? tatooine
-                            : `https://starwars-visualguide.com/assets/img/${type == "people"
-                                ? "characters"
-                                : type}/${data.uid}.jpg`} alt="..." />
+                        <img src={ } alt="..." />
                     </div>
                     <div className="col-md-8">
                         <div className="card-body">
                             <h5 className="card-title">{store.singleItem.properties && store.singleItem.properties.name}</h5>
                             <p className="card-text">{store.singleItem.description}</p>
-                            <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
                         </div>
                     </div>
                 </div>
