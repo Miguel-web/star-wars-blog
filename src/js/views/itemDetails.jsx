9acb410f94174715145e8a997e768bcdf260df13
Miguel-web <miguelgilf@gmail.com>
2023-08-11 18:56:52 +0000

    bootstrap code ready , description in procces

--- src/js/views/itemDetails.jsx
+++ src/js/views/itemDetails.jsx
@@ -11,11 +11,11 @@ export const ItemDetails = (data, type) => {
         actions.getItemDetails(params.type, params.id)
     }, [])
     return (
-        <div className="row aligne-items-center justify-content-center vh-100 bg-dark">
-            <div className="card mb-3 bg-dark" style={{ maxWidth: "540px" }}>
+        <div className="row aligne-items-center justify-content-center vh-50 bg-dark">
+            <div className="card mb-3 bg-dark" style={{ maxWidth: "30rem" }}>
                 <div className="row g-0">
                     <div className="col-md-4">
-                        <img src={`https://starwars-visualguide.com/assets/img/${params.type == "people" ? "characters" : params.type}/${params.id}.jpg`} alt="..." />
+                        <img class="img-fluid rounded-start" src={`https://starwars-visualguide.com/assets/img/${params.type == "people" ? "characters" : params.type}/${params.id}.jpg`} alt="..." />
                     </div>
                     <div className="col-md-8 bg-dark">
                         <div className="card-body bg-dark text-light">
