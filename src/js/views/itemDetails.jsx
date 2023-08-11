5f37b362b03e75611dcf06521dd6c003d3467004
Miguel-web <miguelgilf@gmail.com>
2023-08-11 17:35:04 +0000

    img of itemDetail ready onli need a better bootstrap code

--- src/js/views/itemDetails.jsx
+++ src/js/views/itemDetails.jsx
@@ -15,7 +15,7 @@ export const ItemDetails = (data, type) => {
             <div className="card mb-3 bg-dark" style={{ maxWidth: "540px" }}>
                 <div className="row g-0">
                     <div className="col-md-4">
-                        {/* <img src={ } alt="..." /> */}
+                        <img src={`https://starwars-visualguide.com/assets/img/${params.type == "people" ? "characters" : params.type}/${params.id}.jpg`} alt="..." />
                     </div>
                     <div className="col-md-8 bg-dark">
                         <div className="card-body bg-dark text-light">
