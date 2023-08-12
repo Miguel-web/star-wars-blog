ae63a1785a688e1d442be83165b04f67aa8c2921
Miguel-web <miguelgilf@gmail.com>
2023-08-12 01:49:51 +0000

    ready part, itemdetails planets info not ready

--- src/js/views/itemDetails.jsx
+++ src/js/views/itemDetails.jsx
@@ -12,19 +12,30 @@ export const ItemDetails = (data, type) => {
     }, [])
     return (
         <div className="row aligne-items-center justify-content-center vh-50 bg-dark">
-            <div className="card mb-3 bg-dark" style={{ maxWidth: "30rem" }}>
+            <div className="card mb-3 bg-dark" style={{ maxWidth: "37rem" }}>
                 <div className="row g-0">
                     <div className="col-md-4">
-                        <img class="img-fluid rounded-start" src={`https://starwars-visualguide.com/assets/img/${params.type == "people" ? "characters" : params.type}/${params.id}.jpg`} alt="..." />
+                        <img className="img-fluid rounded-start" src={params.type == "planets" && params.id == 1
+                            ? tatooine
+                            : `https://starwars-visualguide.com/assets/img/${params.type == "people"
+                                ? "characters"
+                                : params.type}/${params.id}.jpg`} alt="..." />
                     </div>
                     <div className="col-md-8 bg-dark">
                         <div className="card-body bg-dark text-light">
-                            <h5 className="card-title">{store.singleItem.properties && store.singleItem.properties.name}</h5>
-                            <p className="card-text">{store.singleItem.description}</p>
+                            <h6>NAME</h6>
+                            <h5 className="card-title text-muted">{store.singleItem.properties && store.singleItem.properties.name}</h5>
+                            <h6>GENDER</h6>
+                            <h5 className="card-text text-muted">{store.singleItem.properties && store.singleItem.properties.gender}</h5>
+                            <h6>Birth Year</h6>
+                            <h5 className="card-text text-muted">{store.singleItem.properties && store.singleItem.properties.birth_year}</h5>
+                            <h6>Eye Color</h6>
+                            <h5 className="card-text text-muted">{store.singleItem.properties && store.singleItem.properties.eye_color}</h5>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
     )
 }
